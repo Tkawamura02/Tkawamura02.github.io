cb38e888522efdd1350ee96c8e1936244f84ebdc
tiffanykawamura <tkawamura02@gmail.com>
2024-05-20 01:46:41 -0700

    FEAT: filtering and responsive projects section

--- script.js
+++ script.js
@@ -12,6 +12,43 @@ function switchTab(evt, name) {
     evt.currentTarget.firstElementChild.className += " tab-border-highlight";
 }
 
+function switchTab2(evt, name) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("tabs2");
+  tablinks = document.getElementsByClassName("tablink2");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" highlight2", "");
+  }
+  evt.currentTarget.firstElementChild.className += " highlight2";
+
+  lowercase_name = name.toLowerCase();
+  console.log(lowercase_name);
+  filterCards(lowercase_name);
+}
+
+function filterCards(category) {
+  const allCards = document.getElementsByClassName('projects-ind-cards');
+  
+  if (category === 'all') {
+    for (let i = 0; i < allCards.length; i++) {
+      allCards[i].style.display = "block";
+      console.log('if loop: ', allCards[i], ' block');
+    }
+  } else {
+    for (let i = 0; i < allCards.length; i++) {
+      const card = allCards[i];
+      if (card.classList.contains(category)) {
+        card.style.display = "block";
+        console.log('if loop: ', card, ' block');
+      } else {
+        card.style.display = "none";
+        console.log('else loop: ', card, ' none');
+      }
+    }
+  }
+}
+
+
 document.addEventListener('DOMContentLoaded', function() {
   var p = document.getElementById('intro-hello');
   p.innerHTML = '';
@@ -36,4 +73,6 @@ document.addEventListener('DOMContentLoaded', function() {
       }, 500);
     }
   }, 60);
+
+  filterCards('all');
 });
